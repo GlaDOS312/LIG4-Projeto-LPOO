befe09bc6d2a77f6fa10021680c60768e0c2adda
GlaDOS <joaocavalcanti2003@gmail.com>
2023-07-30 22:08:25 -0300

    Update Lig4.java

--- Lig4.java
+++ Lig4.java
@@ -2,135 +2,131 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lig4 {
-    private char[][] board;
-    private int currentPlayer;
-    private final char[] players = {'X', 'O'};
-    private final int rows = 6;
-    private final int columns = 7;
+    private char[][] Tabuleiro;
+    private int jogadorAtual;
+    private final char[] Jogadores = {'X', 'O'};
+    private final int Linhas = 6;
+    private final int Colunas = 7;
 
     public Lig4() {
-        board = new char[rows][columns];
-        for (char[] row : board) {
-            Arrays.fill(row, ' ');
+        Tabuleiro = new char[Linhas][Colunas];
+        for (char[] Linha : Tabuleiro) {
+            Arrays.fill(Linha, ' ');
         }
-        currentPlayer = 0;
+        jogadorAtual = 0;
     }
 
     public void play() {
         Scanner scanner = new Scanner(System.in);
-        boolean gameOver = false;
-        int row, col;
+        boolean fimDeJogo = false;
+        int Linha, col;
 
         System.out.println("Bem-vindo ao Lig4!");
 
-        while (!gameOver) {
-            printBoard();
+        while (!fimDeJogo) {
+            printTabuleiro();
 
             do {
-                System.out.printf("Jogador %c, escolha uma coluna (1 a 7): ", players[currentPlayer]);
+                System.out.printf("Jogador %c, escolha uma coluna (1 a 7): ", Jogadores[jogadorAtual]);
                 col = scanner.nextInt() - 1;
-            } while (!isValidMove(col));
+            } while (!movimentoValido(col));
 
-            row = dropPiece(col);
-            if (checkWin(row, col)) {
-                printBoard();
-                System.out.printf("Jogador %c venceu! Parabéns!\n", players[currentPlayer]);
-                gameOver = true;
-            } else if (checkDraw()) {
-                printBoard();
+            Linha = soltarPeca(col);
+            if (checarVitoria(Linha, col)) {
+                printTabuleiro();
+                System.out.printf("Jogador %c venceu! Parabéns!\n", Jogadores[jogadorAtual]);
+                fimDeJogo = true;
+            } else if (checarEmpate()) {
+                printTabuleiro();
                 System.out.println("Empate! O tabuleiro está cheio.");
-                gameOver = true;
+                fimDeJogo = true;
             }
 
-            currentPlayer = (currentPlayer + 1) % 2;
+            jogadorAtual = (jogadorAtual + 1) % 2;
         }
 
         scanner.close();
     }
 
-    private void printBoard() {
+    private void printTabuleiro() {
         System.out.println(" 1 2 3 4 5 6 7");
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < Linhas; i++) {
             System.out.print("|");
-            for (int j = 0; j < columns; j++) {
-                System.out.print(board[i][j] + "|");
+            for (int j = 0; j < Colunas; j++) {
+                System.out.print(Tabuleiro[i][j] + "|");
             }
             System.out.println();
         }
     }
 
-    private boolean isValidMove(int col) {
-        return col >= 0 && col < columns && board[0][col] == ' ';
+    private boolean movimentoValido(int col) {
+        return col >= 0 && col < Colunas && Tabuleiro[0][col] == ' ';
     }
 
-    private int dropPiece(int col) {
-        int row;
-        for (row = rows - 1; row >= 0; row--) {
-            if (board[row][col] == ' ') {
-                board[row][col] = players[currentPlayer];
+    private int soltarPeca(int col) {
+        int Linha;
+        for (Linha = Linhas - 1; Linha >= 0; Linha--) {
+            if (Tabuleiro[Linha][col] == ' ') {
+                Tabuleiro[Linha][col] = Jogadores[jogadorAtual];
                 break;
             }
         }
-        return row;
+        return Linha;
     }
 
-    private boolean checkWin(int row, int col) {
-        char player = players[currentPlayer];
+    private boolean checarVitoria(int Linha, int col) {
+        char player = Jogadores[jogadorAtual];
 
-        // Check horizontal
-        int count = 0;
-        for (int i = Math.max(0, col - 3); i <= Math.min(columns - 1, col + 3); i++) {
-            if (board[row][i] == player) {
-                count++;
-                if (count == 4) return true;
+        int contar = 0;
+        for (int i = Math.max(0, col - 3); i <= Math.min(Colunas - 1, col + 3); i++) {
+            if (Tabuleiro[Linha][i] == player) {
+                contar++;
+                if (contar == 4) return true;
             } else {
-                count = 0;
+                contar = 0;
             }
         }
 
-        // Check vertical
-        count = 0;
-        for (int i = Math.max(0, row - 3); i <= Math.min(rows - 1, row + 3); i++) {
-            if (board[i][col] == player) {
-                count++;
-                if (count == 4) return true;
+        contar = 0;
+        for (int i = Math.max(0, Linha - 3); i <= Math.min(Linhas - 1, Linha + 3); i++) {
+            if (Tabuleiro[i][col] == player) {
+                contar++;
+                if (contar == 4) return true;
             } else {
-                count = 0;
+                contar = 0;
             }
         }
 
-        // Check diagonal (top-left to bottom-right)
-        count = 0;
-        int startRow = row - Math.min(row, col);
-        int startCol = col - Math.min(row, col);
-        for (int i = 0; i < Math.min(rows - startRow, columns - startCol); i++) {
-            if (board[startRow + i][startCol + i] == player) {
-                count++;
-                if (count == 4) return true;
+        contar = 0;
+        int startLinha = Linha - Math.min(Linha, col);
+        int startCol = col - Math.min(Linha, col);
+        for (int i = 0; i < Math.min(Linhas - startLinha, Colunas - startCol); i++) {
+            if (Tabuleiro[Linha + i][startCol + i] == player) {
+                contar++;
+                if (contar == 4) return true;
             } else {
-                count = 0;
+                contar = 0;
             }
         }
 
-        // Check diagonal (top-right to bottom-left)
-        count = 0;
-        startRow = row - Math.min(row, columns - col - 1);
-        startCol = col + Math.min(row, columns - col - 1);
-        for (int i = 0; i < Math.min(rows - startRow, startCol + 1); i++) {
-            if (board[startRow + i][startCol - i] == player) {
-                count++;
-                if (count == 4) return true;
+        contar = 0;
+        startLinha = Linha - Math.min(Linha, Colunas - col - 1);
+        startCol = col + Math.min(Linha, Colunas - col - 1);
+        for (int i = 0; i < Math.min(Linhas - startLinha, startCol + 1); i++) {
+            if (Tabuleiro[startLinha + i][startCol - i] == player) {
+                contar++;
+                if (contar == 4) return true;
             } else {
-                count = 0;
+                contar = 0;
             }
         }
 
         return false;
     }
 
-    private boolean checkDraw() {
-        for (int i = 0; i < columns; i++) {
-            if (board[0][i] == ' ') {
+    private boolean checarEmpate() {
+        for (int i = 0; i < Colunas; i++) {
+            if (Tabuleiro[0][i] == ' ') {
                 return false;
             }
         }
